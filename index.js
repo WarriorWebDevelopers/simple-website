f6bc478f5832df0069fbfdc13a10c23e8b438cca
WarriorWebDevelopers <114642239+WarriorWebDevelopers@users.noreply.github.com>
2022-09-30 10:17:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-let sendButton = document.getElementById("send-button");
-const form = document.getElementById("form");
+const btn = document.getElementById('btn');
 
-sendButton.addEventListener('click', function(e) {
-  e.preventDefault();
-  sendButton.value = 'Sending...';
-  const serviceID = 'service_rvdztkk';
-  const templateID = 'template_5rnsbpi';
-  
-   emailjs.sendForm(serviceID, templateID, form)
-    .then(() => {
-      sendButton.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      sendButton.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
+// ✅ Toggle button text on click
+btn.addEventListener('click', function handleClick() {
+  const initialText = 'Submit';
+
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.textContent = 'Sending...';
+  } else {
+    btn.textContent = initialText;
+  }
 });
+
+/**
+ * ✅ If you need to change the button's inner HTML use:
+ *  - `innerHTML` instead of `textContent`
+ */
+
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    tel: document.getElementById("tel").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_rvdztkk";
+  const templateID = "template_5rnsbpi";
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("tel").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+    })
+    .catch(err=>console.log(err));
+}
