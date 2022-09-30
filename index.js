07669532b6ed496f9b36f5a46ac11ae12f86fbb1
WarriorWebDevelopers <114642239+WarriorWebDevelopers@users.noreply.github.com>
2022-09-30 10:23:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const btn = document.getElementById('btn');
-
-// ✅ Toggle button text on click
-btn.addEventListener('click', function handleClick() {
-  const initialText = 'Submit';
-
-  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
-    btn.textContent = 'Sending...';
-  } else {
-    btn.textContent = initialText;
-  }
-});
-
-/**
- * ✅ If you need to change the button's inner HTML use:
- *  - `innerHTML` instead of `textContent`
- */
-
-
 function sendMail() {
   var params = {
     name: document.getElementById("name").value,
